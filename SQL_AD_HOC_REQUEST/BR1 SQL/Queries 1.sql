f301cf2b03aaa7b899b136ee368d83888b49a3ba
GitikaYadu <gitikayadu7@gmail.com>
2024-12-17 22:13:35 +0530

    Update Queries 1.sql

--- SQL_AD_HOC_REQUEST/BR1 SQL/Queries 1.sql
+++ SQL_AD_HOC_REQUEST/BR1 SQL/Queries 1.sql
@@ -1,9 +1,20 @@
-select ct.city_name,
-count(ft.trip_id) as total_trips,
-sum(ft.fare_amount) / sum(ft.distance_travelled_km) as avg_fare_per_km,
-sum(ft.fare_amount) / count(ft.trip_id) as avg_fare_per_trip,
-round((count(ft.trip_id) / (select count(*)from trips_db.fact_trips)*100),2) as percentage_contribution_to_total_trips
-from trips_db.fact_trips as ft , dim_city as ct
-where ft.city_id = ct.city_id
-group by ct.city_name
-order by count(ft.trip_id) desc ;
+SELECT 
+    ct.city_name,
+    COUNT(ft.trip_id) AS total_trips,
+    SUM(ft.fare_amount) / SUM(ft.distance_travelled_km) AS avg_fare_per_km,
+    SUM(ft.fare_amount) / COUNT(ft.trip_id) AS avg_fare_per_trip,
+    ROUND(
+        (COUNT(ft.trip_id) / 
+        (SELECT COUNT(*) FROM trips_db.fact_trips) * 100), 
+        2
+    ) AS percentage_contribution_to_total_trips
+FROM 
+    trips_db.fact_trips AS ft
+JOIN 
+    trips_db.dim_city AS ct
+ON 
+    ft.city_id = ct.city_id
+GROUP BY 
+    ct.city_name
+ORDER BY 
+    COUNT(ft.trip_id) DESC;
